3d782b3f6d92a4cd06e936e7e217e5418db96656
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 06:50:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path="./lab/emoticons.yml",emoticon)
   end
 end
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning(file_path="./lab/emoticons.yml", emoticon)
    library = load_library(file_path)
   if library["get_meaning"].include?(emoticon) 
     library["get_meaning"][emoticon]
