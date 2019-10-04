f19f7eea14c6eaa105973d9d42f3ee285db62ea1
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 06:43:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path,emoticon)
   # code goes here
   library = File.read(file_path)
   emoji= library.values.find {|faces| faces[0] == emoticon}
+  emoji == nil 
 end
 
 def get_english_meaning
