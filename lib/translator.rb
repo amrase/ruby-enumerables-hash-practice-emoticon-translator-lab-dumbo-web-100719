c34ace63419a64596a111a994b4f35fc0cba507b
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 06:50:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,10 @@ def get_japanese_emoticon(file_path="./lab/emoticons.yml",emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emoji = emoticons.find {|name, faces| faces[1] == emoticon }
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[0]
+   library = load_library(file_path)
+  if library["get_meaning"].include?(emoticon) 
+    library["get_meaning"][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
