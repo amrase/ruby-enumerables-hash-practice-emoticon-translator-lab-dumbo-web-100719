b6a76c19173539bc87458585feb0ccae8b6410d0
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 06:42:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path,emoticon)
   if library.include["get_emoticon"].include?(emoticon)
     library["get_english_meaning"][emoticon]
   else
-      "Sorry"
+      "Sorry, the emoticon was not found"
   end
 end
 
